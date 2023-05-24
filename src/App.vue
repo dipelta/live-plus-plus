7264f4c642fb8d4f2e0ce3179addf71eca229900
dipelta <zha_zha@outlook.com>
2023-05-24 16:46:00 +0800

    feat:删除不要的文件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+  <v-app id="vapp">
+    <router-view id="route-view"></router-view>
+    <v-snackbar v-model="showMsg" min-width="auto" multi-line min-height="0"
+                location="center" :timeout="1500" :color="msgColor">
+      {{ msgContent }}
+    </v-snackbar>
   </v-app>
 </template>
-
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import {defineComponent} from 'vue'
+import {ipcRenderer} from "electron";
 
 export default defineComponent({
   name: 'App',
-
-  components: {
-    HelloWorld,
+  components: {},
+  methods: {},
+  mounted() {
+    console.log(window.location.href)
   },
+  created() {
+    const self = this
 
-  data () {
+    // 监听消息弹窗的事件
+    ipcRenderer.on('alert-msg', function (event, args) {
+      switch (args[0]) {
+        case 'success':
+          self.msgColor = 'green-lighten-1'
+          break;
+        case 'warning':
+          self.msgColor = 'yellow-darken-1'
+          break;
+        case 'error':
+          self.msgColor = 'deep-orange-accent-2'
+          break;
+        default:
+          self.msgColor = 'light-blue-lighten-3'
+          break;
+      }
+      self.msgContent = args[1];
+      self.showMsg = true
+    })
+  },
+  data() {
     return {
-      //
+      showMsg: false,
+      msgContent: "",
+      msgColor: "deep-orange-accent-2"
     }
   },
 })
 </script>
+<style>
+#vapp {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  background: #fff0;
+}
+
+#route-view {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 5px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import {createApp} from 'vue'
 import "./style.css"
 import "video.js/dist/video-js.css"
-// import App from './App.vue'
-import App from './Index.vue'
+import App from './App.vue'
 import router from "./router";
 import vuetify from './plugins/vuetify'
 import {loadFonts} from './plugins/webfontloader'
