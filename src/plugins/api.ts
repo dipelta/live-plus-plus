2a2aee6b9e1ce4976e25c968014f92d1fd856155
dipelta <zha_zha@outlook.com>
2025-07-07 23:41:55 +0800

    feat:更新版本号，更换api地址

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -8,7 +8,7 @@ export class ApiResponse {
 
 class Api {
 
-  private baseURL = 'https://api.live.dipelta.cn/'
+  private baseURL = 'https://api.dipelta.cn/live_app_api/'
 
   // private baseURL = 'http://api.live.me/'
 
