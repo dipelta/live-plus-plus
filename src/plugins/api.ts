eaf7bf13235474628a1eb87bcfa1ebc627c57ebe
dipelta <zha_zha@outlook.com>
2026-09-08 17:31:48 +0800

    feat：修改打包源

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -10,8 +10,7 @@ class Api {
 
   private baseURL = 'https://api.dipelta.cn/live_app_api/'
 
-  // private baseURL = 'http://127.0.0.1:8666/live_app_api/'
-  // private baseURL = 'http://10.0.1.150:8666/live_app_api/'
+  // private baseURL = 'http://0.0.0.0:8666/live_app_api/'
 
   private http() {
     let http = axios.create({
